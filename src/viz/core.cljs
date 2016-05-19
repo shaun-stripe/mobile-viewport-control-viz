409a684a88cc320d3ff25f206d6e1a7e8b0b7ea5
Shaun Williams <shaun@stripe.com>
2016-05-19 13:48:42 -0500

    add animation

--- src/viz/core.cljs
+++ src/viz/core.cljs
@@ -1,4 +1,10 @@
-(ns viz.core)
+(ns viz.core
+  (:require-macros
+    [cljs.core.async.macros :refer [go go-loop]])
+  (:require
+    [cljs.core.async :refer [put! <! chan tap untap mult]]))
+
+(enable-console-print!)
 
 ;;----------------------------------------------------------------------
 ;; Constants
@@ -102,9 +108,89 @@
     (.restore ctx)))
 
 (defn draw []
+  (.clearRect phone-ctx 0 0 phone-width phone-height)
+  (.clearRect space-ctx 0 0 space-width space-height)
   (draw-page-on-phone)
   (draw-page-on-space)
   (draw-phone-on-space))
+
+(defn draw-loop []
+  (draw)
+  (js/requestAnimationFrame draw-loop))
+
+;;----------------------------------------------------------------------
+;; Animation
+;;----------------------------------------------------------------------
+
+(def tick-chan
+  "This channel receives dt (delta time from last frame) in milliseconds."
+  (chan))
+
+(def tick-tap
+  "Allows anything to tap the tick channel (e.g. for animation)."
+  (mult tick-chan))
+
+(def prev-time
+  "Timestamp of the last tick."
+  nil)
+
+(defn tick!
+  "Creates heartbeat by hooking requestAnimationFrame to tick-chan."
+  [curr-time]
+  (let [delta-ms (if prev-time
+                   (- curr-time prev-time)
+                   (/ 1000 60))
+        dt (/ delta-ms 1000)]
+    (set! prev-time curr-time)
+    (put! tick-chan dt))
+  (.requestAnimationFrame js/window tick!))
+
+(defn start-ticking!
+  []
+  (.requestAnimationFrame js/window tick!))
+
+(def tweens
+  ;; find more: https://github.com/danro/jquery-easing/blob/master/jquery.easing.js
+  {:linear identity
+   :swing #(- 0.5 (/ (Math/cos (* % Math/PI)) 2))}) ;; from jquery
+
+(defn resolve-tween
+  "Resolve the tween to a function if it's a name."
+  [tween]
+  (println tween)
+  (cond-> tween
+    (keyword? tween) tweens))
+
+(defn animate!
+  "Pass given animation values to the given callback.
+   Returns a channel that closes when done."
+  [state-path {:keys [a b duration tween] :or {tween :swing} :as opts}]
+  (let [tween (resolve-tween tween)
+        c (chan)
+        resolve-var #(if (= % :_) (get-in @state state-path) %)
+        a (resolve-var a)
+        dv (- b a)]
+    (tap tick-tap c)
+    (go-loop [t 0]
+      (let [dt (<! c)
+            t (+ t dt)
+            percent (-> (/ t duration)
+                        (min 1)
+                        (tween))
+            v (+ a (* percent dv))]
+        (swap! state assoc-in state-path v)
+        (when (< t duration)
+          (recur t)))
+      (untap tick-tap c))))
+
+(defn multi-animate!
+  "Helper for concurrent animations with `animate!`.
+   Returns a channel that closes when all are done."
+  [pairs]
+  (let [anims (mapv #(apply animate! %) pairs)]
+    (go
+      (doseq [a anims]
+        (<! a)))))
 
 ;;----------------------------------------------------------------------
 ;; Init
@@ -140,6 +226,12 @@
   (init-phone)
   (init-frame)
   (init-space)
-  (draw))
+  (draw-loop)
+
+  (start-ticking!)
+  (go-loop []
+    (<! (animate! [:viewport :y] {:a 0 :b 700 :duration 3}))
+    (<! (animate! [:viewport :y] {:a :_ :b 0 :duration 3}))
+    (recur)))
 
 (init)
